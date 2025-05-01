ef437e36cff7a28aff815e7d4e36a5ef51e1bf1e
adam.elkadaoui <adam.elkadaoui@gmail.com>
2025-05-01 18:56:06 +0200

    refactor: add components and format UX/UI

---
+++ src/app/components/key-figures/key-figures.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-figures',
+  standalone: true,
+  imports: [],
+  templateUrl: './key-figures.component.html',
+  styleUrl: './key-figures.component.scss'
+})
+export class KeyFiguresComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { SectorsComponent } from "../../components/sectors/sectors.component";
+import { ServicesComponent } from "../../components/services/services.component";
+import { KeyFiguresComponent } from "../../components/key-figures/key-figures.component";
+import { NewsComponent } from "../../components/news/news.component";
+import { CertificationsComponent } from "../../components/certifications/certifications.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    SectorsComponent,
+    ServicesComponent,
+    KeyFiguresComponent,
+    NewsComponent,
+    CertificationsComponent
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-
+  
 }
